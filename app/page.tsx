afbedf0a8f16dd0a722198c77d84cb5528576871
Mabroor Ahmed <ma942@student.le.ac.uk>
2024-06-04 02:32:11 +0100

    Projects layout cleaned up

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     
     <main className="flex lg:flex-row justify-between lg:py-5  ">
-      
-      <div className="flex flex-row justify-between w-full py-12 border-2 lg:border-purple-800 md:border-red-800 sm:border-green-800 ">
+      {/* border-2 lg:border-purple-800 md:border-red-800 sm:border-green-800  */}
+      <div className="flex flex-row  w-full  ">
         
-        <div className="w-fit border mr-8 ">
+        <div className="w-fit  mr-8 ">
             <div className="text-5xl font-black whitespace-nowrap py-10">
             Full stack developer 
             </div>
@@ -18,7 +18,10 @@ export default function Home() {
             I build things sometimes  
             </div>
         </div>
-        <ProjectsSection/>
+        <div className="">
+          <ProjectsSection/>
+        </div>
+        
         
       </div>
       
--- components/projectsSection.tsx
+++ components/projectsSection.tsx
@@ -8,10 +8,12 @@ import humanizePic from "../public/humanize-portfolio.png"
 export default function ProjectsSection() {
   return (
     <div>
-        <div className="flex flex-col border w-full ">
+        <div className="flex flex-col w-full ">
+            {/* Title */}
           <div className="text-2xl text-right">
             Projects
           </div> 
+          
           <div className="flex flex-row   pt-4 pb-1 pe-2 space-x-2 border-b-2 border-black">
             <Button size="sm"  className=" h-7 rounded-2xl">nextjs</Button>
             <Button size="sm" className=" h-7 rounded-2xl bg-blue-700">React</Button>
@@ -22,42 +24,67 @@ export default function ProjectsSection() {
             <Button size="sm" className=" h-7 rounded-2xl bg-yellow-600">GCP</Button>
 
           </div>
-          <div className="">
-            <div className="flex flex-row pt-5">
-              <div className=" text-white w-3/5 h-48 text-1xl font-bold ">
+          
+          {/* Projects */}
+          <div className="flex flex-col ">
+            {/* Top 2 projects */}
+            <div className="flex flex-row pt-5 h-64">
+              <div className=" w-3/5  ">
 
-              <Link href="/projects">
-                <div className=" w-full h-full">
-                  <div className="text-center text-black">Humanize</div>
-                  <Image
-                    className="border-4 rounded-l-3xl rounded-tr-3xl "
-                    src={humanizePic}
-                    alt="humanize picture"
-                  />
-                  
-                </div>
-              </Link>
+                <Link href="/projects">
+                    <div className=" ">
+                    <div className="text-1xl font-bold  text-center text-black">Humanize</div>
+                    <Image
+                        className="border-2 rounded-l-3xl rounded-tr-3xl "
+                        src={humanizePic}
+                        alt="humanize picture"
+                    />
+                    </div>
+                </Link>
+
               </div>
-              <div className=" w-2/5 h-48  text-1xl font-bold  rounded-tl-3xl  rounded-r-3xl">
-              <Link href="/projects">
-                <div className=" w-full h-full">
-                  <div className="text-center text-black">Finstimulate</div>
-                  <Image
-                    className="border-4 rounded-l-3xl rounded-tr-3xl w-full h-full "
-                    src={humanizePic}
-                    alt="humanize picture"
-                  />
-  
-                </div>
-              </Link>
+              <div className=" w-2/5 ">
+                <Link href="/projects">
+                    <div className=" ">
+                    <div className="text-1xl font-bold text-center text-black">Finstimulate</div>
+                    <Image
+                        className="border-2 rounded-tl-3xl rounded-r-3xl "
+                        src={humanizePic}
+                        alt="humanize picture"
+                    />
+    
+                    </div>
+                </Link>
               </div>
             </div>
-            <div className="flex flex-row pb-5">
-              <div className="bg-gray-400 text-white w-2/5 h-48 p-4 text-2xl font-bold border-4   rounded-l-3xl  rounded-br-3xl">
-              Refugee Aid
+            {/* Bottom 2 projects */}
+            <div className="flex flex-row  h-64">
+              <div className="w-2/5   ">
+                
+                <Link href="/projects">
+                    <div className=" ">
+                    <div className="text-1xl font-bold text-center text-black">Refugee Aid</div>
+                    <Image
+                        className="border-2 rounded-l-3xl rounded-br-3xl "
+                        src={humanizePic}
+                        alt="humanize picture"
+                    />
+    
+                    </div>
+                </Link>
               </div>
-              <div className="bg-gray-400 text-white w-3/5 h-48 p-4 text-2xl font-bold border-4  rounded-bl-3xl  rounded-r-3xl ">
-              Private Cloud Whiteboard
+              <div className=" w-3/5 ">
+                <Link href="/projects">
+                    <div className=" ">
+                    <div className="text-1xl font-bold text-center text-black">Private Cloud whiteboard </div>
+                    <Image
+                        className="border-2 rounded-bl-3xl rounded-r-3xl "
+                        src={humanizePic}
+                        alt="humanize picture"
+                    />
+    
+                    </div>
+                </Link>
               </div>
             </div>
           </div>
