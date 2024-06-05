904380a40f7f06538f7116d2f4958849358994f2
Mabroor <mabroorahmed18@gmail.com>
2024-06-05 03:31:25 +0100

    nav bar padding adjusted

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -13,8 +13,8 @@ export default function NavBar() {
         <div className=" text-1xl">
           Mabroor Ahmed
         </div>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-x-4">
-          <Link href="https://www.linkedin.com/in/mabroor-dev/" className="flex flex-row items-center space-x-1 md:space-x-2 md:px-4">
+        <div className="flex flex-col  md:flex-row  md:space-x-4 ">
+          <Link href="https://www.linkedin.com/in/mabroor-dev/" className="flex flex-row items-center space-x-1 md:space-x-2 md:px-4 ">
             <FaLinkedin />
             <span>LinkedIn</span>
           </Link>
