9e45434b1a231f46f27ed833591f63b10a224fee
Mabroor <mabroorahmed18@gmail.com>
2024-06-06 09:11:55 +0100

    spacing adjusted for main writing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 
 import Image from "next/image";
-import pfpPic from "../public/cropped_pfp.jpeg"
+import pfpPic from "../public/cropped_pfp (1).jpeg"
 import ProjectsSection from "@/components/projectsSection";
 import { PiHandWavingBold } from "react-icons/pi";
 
@@ -24,17 +24,17 @@ export default function Home() {
                 </div>
             </div>
             <div>
-                <div className=" text-3xl lg:text-4xl font-black  py-2 lg:py-10 text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-violet-500 ">
+                <div className=" text-3xl md:text-4xl font-black  py-2 md:py-5 text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-violet-500 ">
                 A Full stack developer 
                 </div>
             </div>
-            <div className=" text-2xl lg:text-3xl text-gray-400 mt-0 lg:mt-5">
+            <div className=" text-2xl md:text-3xl text-gray-400 mt-0  ">
               I build things sometimes  
             </div>
         </div>
 
         <Image
-        className="rounded-full w-28 lg:w-52 lg:mx-0  mt-4"
+        className=" rounded-full w-24 mt-5 lg:mt-0 md:w-52 lg:mx-0   "
         src={pfpPic}
         alt="pfp picture"
 
