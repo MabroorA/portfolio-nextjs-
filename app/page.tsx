1688cb2dc336e3b6ab8114ca1cdd6d3cc00f7d54
Mabroor <mabroorahmed18@gmail.com>
2024-06-13 00:46:43 +0100

    github heatmap and experience added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import pfpPic from "../public/cropped_pfp (1).jpeg"
+import github from "../public/updated-map.png"
 import ProjectsSection from "@/components/projectsSection";
 import { PiHandWavingBold } from "react-icons/pi";
 
@@ -24,21 +25,68 @@ export default function Home() {
                 </div>
             </div>
             <div>
-                <div className=" text-3xl md:text-4xl font-black  py-2 md:py-5 text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-violet-500 ">
+                <div className=" text-3xl md:text-4xl font-black  py-1 md:py-5 text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-violet-500 ">
                 A Full stack developer 
                 </div>
             </div>
             <div className=" text-2xl md:text-3xl text-gray-400 mt-0  ">
               I build things sometimes  
             </div>
+            <div className=" rounded-lg w-60 pt-4  lg:w-80 lg:pt-12">
+              <Image
+              className="rounded-lg"
+              src={github}
+              alt="pfp picture"
+
+              />
+            </div>
         </div>
+        <div className="flex flex-col  ">
+          <div className="pb-2 lg:pb-4">
+            <Image
+            className=" rounded-full w-24 mt-5 lg:mt-0 md:w-52 lg:mx-0  "
+            src={pfpPic}
+            alt="pfp picture"
+            />
+          </div>
+          
+          <div className=" text-teal-500 text-left lg:text-center">
+            Past
+          </div>
+          <div className="flex flex-col text-left  lg:text-center pt-1">
+            <div className="flex flex-col">
+              <div className=" text-gray-300">
+                Web developer intern 
+              </div>
+              <div className=" text-gray-700">
+                Minibikers
+                </div>
 
-        <Image
-        className=" rounded-full w-24 mt-5 lg:mt-0 md:w-52 lg:mx-0   "
-        src={pfpPic}
-        alt="pfp picture"
+            </div>
+            <div className="flex flex-col">
+              <div className=" text-gray-500">
+              Shopify Developer 
+              </div>
+              <div className=" text-gray-700">
+              Miero
+                </div>
 
-        />
+            </div>
+            <div className="flex flex-col">
+              <div className=" text-gray-600">
+              Teaching assistant 
+              </div>
+              <div className=" text-gray-700">
+              University of Leicester
+                </div>
+
+            </div>
+
+            
+          </div>
+          
+
+        </div>
         
         
         
